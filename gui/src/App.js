f40bdeeb2e1a4d1126365ee175e6f5ab5544e1c9
Ktuzinowski <kevinnewtu@outlook.com>
2024-09-19 17:07:06 -0400

    Included the First React App with Map

---
+++ gui/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import MapComponent from './MapComponent';
+
+// Fix marker icon issue
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
+
+function App() {
+  return (
+    <div style={{ height: '100vh', width: '100%' }}>
+      <MapComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ gui/src/MapComponent.js
@@ -0,0 +1,31 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+// Fix marker icon issue
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
+
+const MapComponent = () => {
+  return (
+    <div style={{ height: '100vh', width: '100%' }}>
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{ height: '100%', width: '100%' }}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+};
+
+export default MapComponent;
