06abdc1737488cd6091a8dbf2a40c2ad370fe1e5
ylu <yang.lu.3@stonybrook.edu>
2024-10-03 18:43:16 -0400

    utahstuff

--- gui/src/UtahMap.js
+++ gui/src/UtahMap.js
@@ -1,78 +1,60 @@
-import React, { useEffect, useState } from "react";
-import { MapContainer, TileLayer, GeoJSON, LayersControl } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-import utahCountyData from "./utah_data/utah_counties_cpy.geojson";
+import React, { useEffect, useState } from "react"
+import { MapContainer, TileLayer, GeoJSON, LayersControl } from "react-leaflet"
+import 'leaflet/dist/leaflet.css'
+import utahCongressionalData from "./utah_data/utah_congressional_plan.geojson"
 
-const { Overlay } = LayersControl;
+const { Overlay } = LayersControl
 
 export const UtahMap = () => {
-  const [counties, setCounties] = useState(null);
+    const [congressionalDistricts, setCongressionalDistricts] = useState(null)
 
-  useEffect(() => {
-    fetch(utahCountyData)
-      .then((response) => response.json())
-      .then((data) => {
-        setCounties(data);
-      })
-      .catch((error) =>
-        console.error(
-          "Error loading the Congressional Districts GeoJSON data: ",
-          error
-        )
-      );
-  }, []);
+    useEffect(() => {
+        fetch(utahCongressionalData)
+            .then((response) => response.json())
+            .then((data) => {
+                setCongressionalDistricts(data)
+            })
+        .catch((error) => console.error("Error loading the Congressional Districts GeoJSON data: ", error))
+    }, [])
 
-  const showPopulationData = (feature, layer) => {
-    const popupContent = `
+    const showPopulationData = (feature, layer) => {
+        const popupContent = `
         <div style="font-family: Arial, sans-serif; line-height: 1.5;">
-            <h3 style="margin: 0;">${feature.properties.COUNTY}</h3>
-            <p><strong>Population:</strong> ${feature.properties.TOT_POP22}</p>
-            <p><strong>White:</strong> ${feature.properties.WHT_NHSP22}</p>
-            <p><strong>Black:</strong> ${feature.properties.BLK_NHSP22}</p>
-            <p><strong>Hispanic:</strong> ${feature.properties.HSP_POP22}</p>
-            <p><strong>Asian:</strong> ${feature.properties.ASN_NHSP22}</p>
-            <p><strong>Native:</strong> ${feature.properties.AIA_NHSP22}</p>
-            <p><strong>Pacific:</strong> ${feature.properties.HPI_NHSP22}</p>
-            <p><strong>Other:</strong> ${feature.properties.OTH_NHSP22}</p>
-           
-
+            <h3 style="margin: 0;">District No: ${feature.properties.DISTRICTNO}</h3>
+            <p><strong>Population:</strong> ${feature.properties.TAPERSONS}</p>
+            <p><strong>Voting Age Population:</strong> ${feature.properties.VAPERSONS}</p>
         </div>
         `;
-    layer.bindPopup(popupContent);
-    layer.on("mouseover", function (e) {
-      layer.openPopup(e.latlng);
-    });
+        layer.bindPopup(popupContent);
+    }
 
-    layer.on("mouseout", function () {
-      layer.closePopup();
-    });
-  };
-
-  return (
-    <MapContainer
-      center={[39.32098, -111.093731]} // Center the map on Utah's coordinates
-      zoom={6}
-      style={{ height: "100%", width: "100%" }} // Full screen height (vh = viewport height)
-    >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <LayersControl>
-        <Overlay name="Counties" checked>
-          {counties && (
-            <GeoJSON
-              data={counties}
-              style={() => ({
-                color: "black",
-                fillColor: "white",
-                weight: 2,
-              })}
-              onEachFeature={showPopulationData}
+    return (
+        <MapContainer
+        center={[39.320980, -111.093731]} // Center the map on Utah's coordinates
+        zoom={6}
+        style={{ height: '100%', width: '100%' }}  // Full screen height (vh = viewport height)
+        >
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
-          )}
-        </Overlay>
-      </LayersControl>
-    </MapContainer>
-  );
-};
+            <LayersControl>
+                <Overlay name="Congressional Districts" checked>
+                    {
+                        congressionalDistricts && (
+                            <GeoJSON
+                            data={congressionalDistricts}
+                            style={() => ({
+                                color: 'black',
+                                fillColor: 'white',
+                                weight: 2
+                            })}
+                            onEachFeature={showPopulationData}
+                            />
+                        )
+                    }
+                </Overlay>
+            </LayersControl>
+        </MapContainer>
+    )
+}
