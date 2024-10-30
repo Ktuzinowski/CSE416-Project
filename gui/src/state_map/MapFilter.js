d2fa9a43ef601a04681976e60c1689d14f111ecb
Ktuzinowski <kevinnewtu@outlook.com>
2024-10-30 12:01:53 -0400

    adding updates to the gui

--- gui/src/state_map/MapFilter.js
+++ gui/src/state_map/MapFilter.js
@@ -9,42 +9,53 @@ export const MapFilter = ({ displayCurrent, displaySMD, displayMMD, displayPreci
 
     return (
         <div className="map_filter">
-            <button
-                className={`map_filter_option ${currentSelected === "Current" ? "selected" : ""}`}
-                onClick={() => setCurrentSelected((prevSelected) => {
-                    const newSelection = prevSelected === "Current" ? "" : "Current";
-                    return newSelection
-                })}
-            >
-                Current
-            </button>
-            <button
-                className={`map_filter_option ${smdSelected === "SMD" ? "selected" : ""}`}
-                onClick={() => setSmdSelected((prevSelected) => {
-                    const newSelection = prevSelected === "SMD" ? "" : "SMD";
-                    return newSelection;
-                })}
-            >
-                SMD
-            </button>
-            <button
-                className={`map_filter_option ${mmdSelected === "MMD" ? "selected" : ""}`}
-                onClick={() => setMmdSelected((prevSelected) => {
-                    const newSelection = prevSelected === "MMD" ? "" : "MMD";
-                    return newSelection;
-                })}
-            >
-                MMD
-            </button>
-            <button
-                className={`map_filter_option ${precinctsSelected === "Precincts" ? "selected" : ""}`}
-                onClick={() => setPrecinctsSelected((prevSelected) => {
-                    const newSelection = prevSelected === "Precincts" ? "" : "Precincts";
-                    return newSelection
-                })}
-            >
-                Precincts
-            </button>
+            <span>
+                <button
+                    style={{borderRight: "None", borderRadius: "3px 0px 0px 3px"}}
+                    className={`map_filter_option ${currentSelected === "Current" ? "selected" : ""}`}
+                    onClick={() => setCurrentSelected((prevSelected) => {
+                        const newSelection = prevSelected === "Current" ? "" : "Current";
+                        return newSelection
+                    })}
+                >
+                    Current
+                </button>
+            </span>
+            <span>
+                <button
+                    style={{borderRight: "None"}}
+                    className={`map_filter_option ${smdSelected === "SMD" ? "selected" : ""}`}
+                    onClick={() => setSmdSelected((prevSelected) => {
+                        const newSelection = prevSelected === "SMD" ? "" : "SMD";
+                        return newSelection;
+                    })}
+                >
+                    SMD
+                </button>
+            </span>
+            <span>
+                <button
+                    className={`map_filter_option ${mmdSelected === "MMD" ? "selected" : ""}`}
+                    onClick={() => setMmdSelected((prevSelected) => {
+                        const newSelection = prevSelected === "MMD" ? "" : "MMD";
+                        return newSelection;
+                    })}
+                >
+                    MMD
+                </button>
+            </span>
+            <span>
+                <button
+                    style={{borderLeft: "None", borderRadius: "0px 3px 3px 0px"}}
+                    className={`map_filter_option ${precinctsSelected === "Precincts" ? "selected" : ""}`}
+                    onClick={() => setPrecinctsSelected((prevSelected) => {
+                        const newSelection = prevSelected === "Precincts" ? "" : "Precincts";
+                        return newSelection
+                    })}
+                >
+                    Precincts
+                </button>
+            </span>
         </div>
     );
 };
