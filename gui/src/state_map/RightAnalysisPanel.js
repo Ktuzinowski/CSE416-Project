883e147193767feaa244062fcbda41afd5f56651
Ktuzinowski <kevinnewtu@outlook.com>
2024-11-21 10:56:40 -0500

    adding different paths

--- gui/src/state_map/RightAnalysisPanel.js
+++ gui/src/state_map/RightAnalysisPanel.js
@@ -4,9 +4,9 @@ import { useState } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faExpandAlt, faCompressAlt } from '@fortawesome/free-solid-svg-icons';
 import { RightAnalysisPanelOptions, RightAnalysisEnsembleOptions, RightAnalysisSearchOptions, RightAnalysisSummaryOptions } from "../utils/Constants";
-import { EnsembleSummarySMD } from "./components/summary/EnsembleSummarySMD";
-import { EnsembleSummaryMMD } from "./components/summary/EnsembleSummaryMMD";
-import { EnsembleSummaryCompare } from "./components/summary/EnsembleSummaryCompare";
+import { EnsembleSummarySMD } from "./components/ensemble/EnsembleSummarySMD";
+import { EnsembleSummaryMMD } from "./components/ensemble/EnsembleSummaryMMD";
+import { EnsembleSummaryCompare } from "./components/ensemble/EnsembleSummaryCompare";
 
 export const RightAnalysisPanel = ({ setIsRightAnalysisPanelExpanded }) => {
     const [isExpanded, setIsExpanded] = useState(false);
