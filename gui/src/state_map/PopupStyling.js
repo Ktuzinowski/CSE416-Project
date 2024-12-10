b573c8808d9f28a5333c544c860f5b7ea5827c17
ylu <yang.lu.3@stonybrook.edu>
2024-12-09 22:56:16 -0500

    ADDED COMMA NOTATION

--- gui/src/state_map/PopupStyling.js
+++ gui/src/state_map/PopupStyling.js
@@ -1,11 +1,11 @@
-import { CurrentDistrictPlansFeatureProperties, PrecinctsFeatureProperties } from "../utils/MongoDocumentProperties"
+import { CurrentDistrictPlansFeatureProperties, PrecinctsFeatureProperties } from "../utils/MongoDocumentProperties";
 
 export const getDistrictDataPopupContent = (feature) => {
     const popupContent = `
     <div style="font-family: Arial, sans-serif; line-height: 1.5;">
         <h3 style="margin: 0;">District No: ${feature.properties[CurrentDistrictPlansFeatureProperties.district]}</h3>
         <p><strong>Representative:</strong> ${feature.properties[CurrentDistrictPlansFeatureProperties.representative]}</p>
-        <p><strong>Population:</strong> ${feature.properties[CurrentDistrictPlansFeatureProperties.population]}</p>
+        <p><strong>Population:</strong> ${feature.properties[CurrentDistrictPlansFeatureProperties.population].toLocaleString()}</p>
 
         <p><strong>Republican:</strong> ${(
           (feature.properties[CurrentDistrictPlansFeatureProperties.republican] / (feature.properties[CurrentDistrictPlansFeatureProperties.republican] + feature.properties[CurrentDistrictPlansFeatureProperties.democrat])) *
@@ -48,51 +48,50 @@ export const getDistrictDataPopupContent = (feature) => {
     </div>
     `;
     return popupContent;
-}
+};
 
 export const getPrecinctDataPopupContent = (feature) => {
     const popupContent = `
     <div style="font-family: Arial, sans-serif; line-height: 1.5;">
-        <h3 style="margin: 0;">Precinct: ${
-          feature.properties[PrecinctsFeatureProperties.precinct]}</h3>
-        <p><strong>Population:</strong> ${feature.properties[PrecinctsFeatureProperties.population]}</p>
-    <p><strong>Republican:</strong> ${(
-      (feature.properties[PrecinctsFeatureProperties.republican] / (feature.properties[PrecinctsFeatureProperties.republican] + feature.properties[PrecinctsFeatureProperties.democrat])) *
-      100
-    ).toFixed(2)}%</p>
-    <p><strong>Democrat:</strong> ${(
-      (feature.properties[PrecinctsFeatureProperties.democrat] / (feature.properties[PrecinctsFeatureProperties.republican] + feature.properties[PrecinctsFeatureProperties.democrat])) *
-      100
-    ).toFixed(2)}%</p>
-    <p><strong>White:</strong> ${(
-      (feature.properties[PrecinctsFeatureProperties.white] / feature.properties[PrecinctsFeatureProperties.population]) *
-      100
-    ).toFixed(2)}%</p>
-    <p><strong>Black:</strong> ${(
-      (feature.properties[PrecinctsFeatureProperties.black] / feature.properties[PrecinctsFeatureProperties.population]) *
-      100
-    ).toFixed(2)}%</p>
-    <p><strong>Hispanic:</strong> ${(
-      (feature.properties[PrecinctsFeatureProperties.hispanic] / feature.properties[PrecinctsFeatureProperties.population]) *
-      100
-    ).toFixed(2)}%</p>
-    <p><strong>Asian:</strong> ${(
-      (feature.properties[PrecinctsFeatureProperties.asian] / feature.properties[PrecinctsFeatureProperties.population]) *
-      100
-    ).toFixed(2)}%</p>
-    <p><strong>Pacific:</strong> ${(
-      (feature.properties[PrecinctsFeatureProperties.pacific] / feature.properties[PrecinctsFeatureProperties.population]) *
-      100
-    ).toFixed(2)}%</p>
-    <p><strong>Indigenous:</strong> ${(
-      (feature.properties[PrecinctsFeatureProperties.indigenous] / feature.properties[PrecinctsFeatureProperties.population]) *
-      100
-    ).toFixed(2)}%</p>
-    <p><strong>Other:</strong> ${(
-      (feature.properties[PrecinctsFeatureProperties.other] / feature.properties[PrecinctsFeatureProperties.population]) *
-      100
-    ).toFixed(2)}%</p>
+        <h3 style="margin: 0;">Precinct: ${feature.properties[PrecinctsFeatureProperties.precinct]}</h3>
+        <p><strong>Population:</strong> ${feature.properties[PrecinctsFeatureProperties.population].toLocaleString()}</p>
+        <p><strong>Republican:</strong> ${(
+          (feature.properties[PrecinctsFeatureProperties.republican] / (feature.properties[PrecinctsFeatureProperties.republican] + feature.properties[PrecinctsFeatureProperties.democrat])) *
+          100
+        ).toFixed(2)}%</p>
+        <p><strong>Democrat:</strong> ${(
+          (feature.properties[PrecinctsFeatureProperties.democrat] / (feature.properties[PrecinctsFeatureProperties.republican] + feature.properties[PrecinctsFeatureProperties.democrat])) *
+          100
+        ).toFixed(2)}%</p>
+        <p><strong>White:</strong> ${(
+          (feature.properties[PrecinctsFeatureProperties.white] / feature.properties[PrecinctsFeatureProperties.population]) *
+          100
+        ).toFixed(2)}%</p>
+        <p><strong>Black:</strong> ${(
+          (feature.properties[PrecinctsFeatureProperties.black] / feature.properties[PrecinctsFeatureProperties.population]) *
+          100
+        ).toFixed(2)}%</p>
+        <p><strong>Hispanic:</strong> ${(
+          (feature.properties[PrecinctsFeatureProperties.hispanic] / feature.properties[PrecinctsFeatureProperties.population]) *
+          100
+        ).toFixed(2)}%</p>
+        <p><strong>Asian:</strong> ${(
+          (feature.properties[PrecinctsFeatureProperties.asian] / feature.properties[PrecinctsFeatureProperties.population]) *
+          100
+        ).toFixed(2)}%</p>
+        <p><strong>Pacific:</strong> ${(
+          (feature.properties[PrecinctsFeatureProperties.pacific] / feature.properties[PrecinctsFeatureProperties.population]) *
+          100
+        ).toFixed(2)}%</p>
+        <p><strong>Indigenous:</strong> ${(
+          (feature.properties[PrecinctsFeatureProperties.indigenous] / feature.properties[PrecinctsFeatureProperties.population]) *
+          100
+        ).toFixed(2)}%</p>
+        <p><strong>Other:</strong> ${(
+          (feature.properties[PrecinctsFeatureProperties.other] / feature.properties[PrecinctsFeatureProperties.population]) *
+          100
+        ).toFixed(2)}%</p>
     </div>
     `;
     return popupContent;
-}
+};
