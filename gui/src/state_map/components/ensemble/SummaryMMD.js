bb97da70fa797410398b9dbbb52a66310d65aae8
ylu <yang.lu.3@stonybrook.edu>
2024-12-11 20:26:29 -0500

    gui 4 is complete

--- gui/src/state_map/components/ensemble/SummaryMMD.js
+++ gui/src/state_map/components/ensemble/SummaryMMD.js
@@ -11,7 +11,25 @@ export const SummaryMMD = ({ state }) => {
         };
         loadSummaryData(state);
     }, [state])
-    return (
-        <h1>Summary MMD</h1>
-    )
+
+    if(summaryData != null){
+        return (
+            <>
+    
+            <h1 className="summarySMDHeader">Summary MMD</h1>
+    
+            <div className="summaryData">
+            <p>State: {summaryData.state.charAt(0).toUpperCase() + summaryData.state.slice(1)}</p>
+            <p>Number of Plans: {summaryData.num_plans}</p>
+            <p>Minority Representatives: {summaryData.minority_reps}</p>
+            <p>Population Deviation: {summaryData.pop_dev}</p>
+            <p>Republican Share: {summaryData.rep_share}%</p>
+            <p>Democratic Share: {summaryData.dem_share}%</p>
+            <p>MMD Layout: {summaryData.mmd_layout}</p>
+            
+            </div>
+    
+            </>
+        )
+    }
 }
