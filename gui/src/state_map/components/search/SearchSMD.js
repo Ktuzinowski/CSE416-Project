a4791ba9ea76a10269968ef96146066d426afa66
Ktuzinowski <kevinnewtu@outlook.com>
2024-12-13 09:00:02 -0500

    worked on updating the search component

--- gui/src/state_map/components/search/SearchSMD.js
+++ gui/src/state_map/components/search/SearchSMD.js
@@ -16,18 +16,18 @@ export const SearchSMD = ({ state }) => {
     <div className="right_data_panel_current_selection">
         <h2 className="right_data_panel_title">{`SMD Search ${state.charAt(0).toUpperCase() + state.slice(1)}`}</h2>
     </div>
-    <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+    <div style={{ display: "flex", marginLeft: "30px" }}>
     {/* Render the grid if summaries are available */}
     {summaries && (
         <div
             style={{
                 display: "grid",
                 gridTemplateColumns: "repeat(2, 1fr)", // Always 2 columns
-                gap: "18px",
+                gap: "15px",
                 alignItems: "center",
                 justifyContent: "center",
                 width: "100%", // Ensures the grid spans full available width
-                maxWidth: "500px", // Optional: Limits the total width of the grid
+                maxWidth: "100%", // Optional: Limits the total width of the grid
             }}
         >
             {summaries.map((summary, index) => (
