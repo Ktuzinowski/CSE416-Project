626baff03586600462e0eb5ce012508a32eee521
Ktuzinowski <kevinnewtu@outlook.com>
2024-12-19 16:16:34 -0500

    updates

--- gui/src/state_map/components/summary/smd/SmdElectionResults.js
+++ gui/src/state_map/components/summary/smd/SmdElectionResults.js
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
-import { getSmdDistrictPlanElections } from "../../../../axiosClient";
+import { getSmdDistrictPlanElections, getMmdDistrictPlanSummary } from "../../../../axiosClient";
 
 export const SmdElectionResults = ({ name, nameMmd }) => {
   const [electionData, setElectionData] = useState(null);
+  const [mmdElectionData, setMmdElectionData] = useState(null);
 
   useEffect(() => {
     const loadData = async (name) => {
@@ -14,7 +15,13 @@ export const SmdElectionResults = ({ name, nameMmd }) => {
     if (name) {
       loadData(name);
     }
-  }, [name]);
+    const loadDataMmd = async (nameMmd) => {
+      const data = await getMmdDistrictPlanSummary(nameMmd);
+      setMmdElectionData(data);
+      console.log(data);
+    }
+    loadDataMmd(nameMmd);
+  }, [name, nameMmd]);
 
   if (!electionData) {
     return <p>Loading...</p>;
@@ -30,9 +37,20 @@ export const SmdElectionResults = ({ name, nameMmd }) => {
     { label: "Loser-Party", key: (feature) => feature.properties.loser_party },
   ];
 
+  const categoriesMmd = [
+    { label: "Vote Totals", key: (feature) => (feature.properties.republican + feature.properties.democrat).toLocaleString() },
+    { label: "Republican", key: (feature) => feature.properties.republican.toLocaleString() },
+    { label: "Democratic", key: (feature) => feature.properties.democrat.toLocaleString() },
+    // { label: "Representative", key: (feature) => feature.properties.representative },
+    // { label: "Incumbent Party", key: (feature) => feature.properties.incumbent },
+    // { label: "Loser", key: (feature) => feature.properties.loser },
+    // { label: "Loser-Party", key: (feature) => feature.properties.loser_party },
+  ];
+
   return (
     <div className="supp">
-      <p style={{fontSize: "15px"}}><b>Category:</b> {electionData.interesting_description}</p>
+      <p style={{fontSize: "15px", marginTop: "-25px"}}><b>Category SMD:</b> {electionData.interesting_description}</p>
+      <p style={{fontSize: "15px"}}><b>Category MMD:</b> {mmdElectionData.interesting_description}</p>
       <table style={{ width: "100%", borderCollapse: "collapse" }}>
         <thead>
           <tr>
@@ -47,6 +65,26 @@ export const SmdElectionResults = ({ name, nameMmd }) => {
             <tr key={index}>
               <td style={{ border: "1px solid black", padding: "2px" }}>{category.label}</td>
               {electionData.features.map((feature, featureIndex) => (
+                <td key={featureIndex} style={{ border: "1px solid black", padding: "2px" }}>{category.key(feature)}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <table style={{ width: "100%", borderCollapse: "collapse", marginTop: "5px" }}>
+        <thead>
+          <tr>
+            <th style={{ border: "1px solid black", padding: "2px" }}>Category</th>
+            {mmdElectionData.features.map((_, index) => (
+              <th key={index} style={{ border: "1px solid black", padding: "2px" }}>District {index + 1}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {categoriesMmd.map((category, index) => (
+            <tr key={index}>
+              <td style={{ border: "1px solid black", padding: "2px" }}>{category.label}</td>
+              {mmdElectionData.features.map((feature, featureIndex) => (
                 <td key={featureIndex} style={{ border: "1px solid black", padding: "2px" }}>{category.key(feature)}</td>
               ))}
             </tr>
